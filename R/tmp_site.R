d892d505c044dc98a7ec5f3c1576d96851b3a751
John Blischak <jdblischak@gmail.com>
2018-02-04 16:47:34 -0500

    Use R's tempdir(), suppress messages, and cleanup.
    * Use tempdir() to make it cross-platform
    * Suppress messages so that only last file is previewed
    * Cleanup since these directories could potentially be large

--- R/tmp_site.R
+++ R/tmp_site.R
@@ -1,8 +1,8 @@
 
 
-#' Custom site generator to render in /tmp
+#' Custom site generator to render in in a temporary directory
 #'
-#' To render your R Markdown documents in \code{/tmp}, you need to add a file
+#' To render your R Markdown documents in a temporary directory, you need to add a file
 #' \code{index.Rmd} that specifies \code{site: tmpsite::tmp_site} in the YAML
 #' header.
 #'
@@ -20,24 +20,28 @@ tmp_site <- function(input, encoding = getOption("encoding"), ...) {
                      quiet,
                      encoding, ...) {
 
-    input_file <- normalizePath(input_file)
-    # Copy the entire project to /tmp
-    dir_proj <- dirname(input_file)
+    input_file_w_path <- normalizePath(input_file)
+    # Copy the entire project to temporary directory
+    dir_proj <- dirname(input_file_w_path)
     dir_upstream <- dirname(dir_proj)
-    dir_tmp <- "/tmp"
+    dir_tmp <- tempfile("tmp_site-")
+    dir.create(dir_tmp)
     dir_new <- file.path(dir_tmp, basename(dir_proj))
     file.copy(from = dir_proj, to = dir_tmp, recursive = TRUE, overwrite = TRUE)
     file_tmp <- file.path(dir_new, basename(input_file))
 
     # Render the file
-    rmarkdown::render(file_tmp,
+    suppressMessages(rmarkdown::render(file_tmp,
                       output_format = output_format,
                       envir = envir,
                       quiet = quiet,
-                      encoding = encoding)
+                      encoding = encoding))
 
     # Copy the entire project back
     file.copy(from = dir_new, to = dir_upstream, recursive = TRUE, overwrite = TRUE)
+
+    # Remove temporary directory
+    unlink(dir_tmp, recursive = TRUE)
 
     # Open in RStudio Viewer
     if (!quiet) {
